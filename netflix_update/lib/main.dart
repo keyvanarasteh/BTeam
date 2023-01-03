ee59ecc72bff3d8a52b8ad5c8dd6617fdcd4dfd3
mirayunal <222016762@stu.istinye.edu.tr>
2023-01-03 18:03:28 +0300

    netflix update

---
+++ netflix_update/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/desktop.dart';
+import 'package:netflix/large.dart';
+import 'package:netflix/screen.dart';
+import 'package:netflix/tablet.dart';
+
+import 'mobile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Netflix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const screen(),
+    );
+  }
+}
